de2758a23c93f0999d4c63cf58e6846f846fa94e
Jeffrey Chan <jeffreychan1303@gmail.com>
2023-12-07 15:42:39 -0500

    feature: add routs, and controllers for the simple database queries

--- server/src/controllers/controllers.ts
+++ server/src/controllers/controllers.ts
@@ -32,3 +32,79 @@ export const getAllSubmissions = async (req: Request, res: Response) => {
     console.log(err);
   }
 };
+
+export const createBreak = async (req: Request, res: Response) => {
+  try {
+    const response = await database.insertBreak(
+      req.body.session_id,
+      req.body.has_accepted,
+      req.body.duration
+    );
+    res.send(response);
+    return response;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const createSubmission = async (req: Request, res: Response) => {
+  try {
+    const response = await database.insertSubmission(
+      req.body.session_id,
+      req.body.patient_id,
+      req.body.interpretation,
+      req.body.last_interaction,
+      req.body.is_valid
+    );
+    res.send(response);
+    return response;
+  } catch (err) {
+    console.log(err);
+  }
+};
+export const createSession = async (req: Request, res: Response) => {
+  try {
+    const response = await database.insertSession(
+      req.body.participant_number,
+      req.body.duration
+    );
+    res.send(response);
+    return response;
+  } catch (err) {
+    console.log(err);
+  }
+};
+export const createParticipant = async (req: Request, res: Response) => {
+  try {
+    const response = await database.insertParticipant(
+      req.body.participant_number,
+      req.body.full_name,
+      req.body.task_duration,
+      req.body.break_duration,
+      req.body.break_count_interval,
+      req.body.break_time_interval
+    );
+    res.send(response);
+    return response;
+  } catch (err) {
+    console.log(err);
+  }
+};
+export const updateParticipantSettings = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const response = await database.updateParticipantSettings(
+      req.body.task_duration,
+      req.body.break_duration,
+      req.body.break_count_interval,
+      req.body.break_time_interval,
+      req.body.participant_number
+    );
+    res.send(response);
+    return response;
+  } catch (err) {
+    console.log(err);
+  }
+};
--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -4,9 +4,14 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', controllers.getTables);
-
 router.get('/participantById/:id', controllers.getParticipantById);
-
 router.get('/allSubmissions', controllers.getAllSubmissions);
+
+router.post('/createBreak', controllers.createBreak);
+router.post('/createSubmission', controllers.createSubmission);
+router.post('/createSession', controllers.createSession);
+router.post('/createParticipant', controllers.createParticipant);
+
+router.put('/updateParticipantSettings', controllers.updateParticipantSettings);
 
 export default router;
